e56e518ddc27370d21990807b3f06443823e3ee5
cheol ryu <cheolryu6118@gmail.com>
2022-08-08 09:54:53 -0700

    08082022

--- src/containers/WhatWeDo/WhatWeDo.jsx
+++ src/containers/WhatWeDo/WhatWeDo.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Feature } from '../../components';
 import './what.css'
 const WhatWeDo = () => {
   return (
-    <div>WhatWeDo</div>
+    <div className="gpt3__whatgpt3 section__margin" id='whpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="What is GPT-3" text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagenation.</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title="Chatbot" text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
+        <Feature title="Knowledge" text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
+        <Feature title="Education" text="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
+      </div>
+    </div>
   )
 }
 
